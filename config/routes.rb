8a426e95435335b3cc4577e82a80d95bd7584d9e
aomanovic <ao15155@etf.unsa.ba>
2015-03-25 23:34:04 +0100

    initial

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  devise_for :users
-  root to: "home#index"
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -58,5 +53,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
 end
